9af451e76f84293a1b6d3a5f4528aeabb48c8c04
sespinosav <sespinosav@eafit.edu.co>
2020-07-04 17:29:37 -0500

    "Agregando comentarios al codigo factorial.py"

--- factorial.py
+++ factorial.py
@@ -1,6 +1,14 @@
+"""
+
+Funcion recursiva para calcular el factorial
+de un numero
+
+"""
+
 def factorial(n):
     if n == 1:
         return 1
     return n*factorial(n-1)
 num = int(input("ingrese el numero"))
+
 print(factorial(num))
