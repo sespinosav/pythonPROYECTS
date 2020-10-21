bd08063e737a73fe74777429dcaa9c1f1d9c21ac
sespinosav <sespinosav@github.com>
2020-10-20 19:37:24 -0500

    Add test 0 for newton method

--- analisis_numerico/newton.py
+++ analisis_numerico/newton.py
@@ -1,44 +1,11 @@
-"""
-read tol, x0, niter
-fx = f(x0)
-dfx = f'(x0)
-contador = 0
-error = 0
-error = tol + 1
-
-while err > tol and fx != 0 and dfx != 0 and contador < niter do
-    x1 = x0 - fx/dfx
-    fx = f'(x1)
-    dfx = f'(x1)
-    error = abs(x1 - x0)
-    x0 = x1
-    contador = contador + 1
-end while
-if fx = 0 then
-    x0 es raiz
-else if error < tol then
-    x1 es es aproximacion a una raiz con una tolerancia = tol
-else if dfx = 0 then
-    x1 es una posible raiz multiple
-else
-    fracaso en niter iteraciones
-end if
-"""
-
-from function import function as fun
-import math
-
-f = input("Ingrese la funcion: ")
-dfx = input("Ingrese la derivada de la funcion: ")
+f = eval(input("Ingrese la funcion: "))
+df = eval(input("Ingrese la derivada de la funcion: "))
 tol = float(input("Ingrese la tolerancia: "))
 x0 = float(input("Ingrese x0: "))
 niter = float(input("Ingrese el maximo de iteraciones: "))
 
-f = fun(f)
-df = fun(dfx)
-
-fx = f.evaluate(x0)
-dfx = df.evaluate(x0)
+fx = f(x0)
+dfx = df(x0)
 
 count = 0
 err = 0
@@ -46,16 +13,18 @@ err = tol + 1
 
 while (err > tol) and (fx != 0) and (dfx != 0) and (count < niter):
     x1 = x0 - fx/dfx
-    fx = f.evaluate(x1)
-    dfx = df.evaluate(x1)
-    err = math.fabs(x1 - x0)
+    fx = f(x1)
+    dfx = df(x1)
+    err = abs(x1 - x0)
     x0 = x1
     count += 1
-if fx = 0:
+    print("i: ",count,"x0: ",x0,"x1: ",x1,"e: ",err)
+print()
+if fx == 0:
     print("x0 es raiz",x0)
 elif err < tol:
-    print("x1 es aproximacion a una raiz con una tolerancia:",tol)
-elif dfx = 0:
+    print("x1 es aproximacion a una raiz con una tolerancia:",x1, tol)
+elif dfx == 0:
     print("x1 es una posible raiz multiple",x1)
 else:
     print("Fracaso en " + niter + " iteraciones")
