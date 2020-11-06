b7d7a567a355c367d611a130c3458e98e81ea48f
sespinosav <sespinosav@eafit.edu.co>
2020-11-06 17:01:23 -0500

    add methods

--- analisis_numerico/gaussianaPivoteoTotal.py
+++ analisis_numerico/gaussianaPivoteoTotal.py
@@ -1,9 +1,9 @@
-from gaussiana import eliminacionGaussianaConPivoteo, sustitucionRegresiva
+from gaussiana import eliminacionGaussianaConPivoteo, sustitucionRegresiva, reordenar
 
 A = eval(input("Ingrese la matriz A: "))
 b = eval(input("Ingrese el arreglo b: "))
 
-Ab= eliminacionGaussianaConPivoteo(A, b, len(A),"")
+Ab, marcas = eliminacionGaussianaConPivoteo(A, b, len(A),"")
 x = sustitucionRegresiva(Ab, len(A))
-
+x = reordenar(x, marcas)
 print(x)
