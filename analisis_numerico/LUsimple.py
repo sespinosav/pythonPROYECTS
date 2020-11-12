e04568895744b48761d3bef8d44f5548efdc0d86
sespinosav <sespinosav@eafit.edu.co>
2020-11-11 23:51:42 -0500

    add prints

--- analisis_numerico/LUsimple.py
+++ analisis_numerico/LUsimple.py
@@ -2,23 +2,63 @@ from gaussiana import formaMatrizAumentada, sustitucionProgresiva, sustitucionRe
 
 A = eval(input("Ingrese A: "))
 b = eval(input("Ingrese b: "))
+etapa = 0
 
-L = [[0 for i in range(len(A))] for j in range(len(A))]
+L = [[0.0 for i in range(len(A))] for j in range(len(A))]
+A = [[float(i) for i in j] for j in A]
 U = []
-def factorizacionLU(A, b, n):
+def factorizacionLU(A, b, n, etapa):
     Ab = formaMatrizAumentada(A,b)                      
     for k in range(n-1):
-        L[k][k] = 1
+        L[k][k] = 1.0
         for i in range(k+1, n):
             L[i][k] = multiplicador = Ab[i][k] / Ab[k][k]
             for j in range(k, n+1):
                 Ab[i][j] -= (multiplicador * Ab[k][j])
-    L[len(L)-1][len(L)-1] = 1
+        etapa += 1
+        print()
+        print(f"Etapa {etapa}")
+        print()
+        print("L:")
+        for i in L:
+            result = ""
+            for j in i:
+                text = str(j)
+                text += "000000"
+                result += text[:7]+" "
+            print(result)
+        print()
+    L[len(L)-1][len(L)-1] = 1.0
+
     return Ab
     
-Ab = factorizacionLU(A,b, len(A))
+print("LU con gaussiana simple:")
+print()
+print("Resultados")
+print()
+print(f"Etapa {etapa}")
+print()
+for i in A:
+    result = ""
+    for j in i:
+        text = str(j)
+        text += "0000"
+        result += text[:7]+" "
+    print(result)
+print()
+Ab = factorizacionLU(A,b, len(A),etapa)
 for i in Ab:
     U.append(i[:len(i)-1])
+
+print("U:")
+for i in [i[:len(i)-1] for i in Ab]:
+    result = ""
+    for j in i:
+        text = str(j)
+        text += "0000"
+        result += text[:7]+" "
+    print(result)
+print()
 
 Lb = formaMatrizAumentada(L,b)
 z = sustitucionProgresiva(Lb,len(L))
--- analisis_numerico/sor.py
+++ analisis_numerico/sor.py
@@ -12,13 +12,11 @@ disp = Tol + 1
 
 def calcularNuevoSor(x0):
     for i in range(len(A)):
-        x1[i] = x0[i]
-    for i in range(len(A)):
         sum1 = 0
         for j in range(len(A)):
             if j != i:
                 sum1 += A[i][j]*x1[j]
-        x1[i] = ((1-w)*x0[i])-(w*(b[i] - sum1)/A[i][i])
+        x1[i] = ((1-w)*x0[i])+(w*(b[i] - sum1)/A[i][i])
     return x1
 
 def norma(x1,x0):
@@ -38,7 +36,7 @@ while disp > Tol and count < Nmax:
     disp = (norma(x1,x0))/norma1(x1)
     x0 = [i for i in x1]    
     count += 1
-    print(count)
+    print(count,x1)
 if disp < Tol:
     print(f"{x1} es aproximacion con una tolerancia = {Tol}")
 else:
